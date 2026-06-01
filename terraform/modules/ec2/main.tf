0148272a558cc3a9d104024c37d44f12a683c25d
Shreeraksha-18 <shreerakshasjadhavbsc24@rvu.edu.in>
2026-06-01 21:20:52 +0530

    trigger deployment

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -58,7 +58,7 @@ USERDATA
 
 resource "aws_instance" "app" {
   ami                         = var.ami_id
-  instance_type               = "t2.micro"
+  instance_type               = "t3.micro"
   subnet_id                   = var.subnet_id
   vpc_security_group_ids      = [aws_security_group.ec2.id]
   associate_public_ip_address = true
